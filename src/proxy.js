753e0073e0f9d42be1c5372f50c9cb14817a67e1
merajhos <meraj357685@gmail.com>
2026-08-28 14:22:15 +0600

    added something

--- src/proxy.js
+++ src/proxy.js
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 import { auth } from './lib/auth'
 import { headers } from 'next/headers'
  
-// This function can be marked `async` if using `await` inside
+e
 export async function proxy(request) {
     const session = await auth.api.getSession({
         headers: await headers()
